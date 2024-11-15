38841032a926d9ff9d7ba63e425adc913fde3bbe
anikabushrapriyanka <anikabushrapriyanka@gmail.com>
2024-11-15 09:41:33 +0600

    Add files via upload

---
+++ Labtask2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+     int num;
+
+     cout<<"Enter any integer number : ";
+
+     cin>>num;
+
+     if(num>0){
+        cout<<"It is a positive number"<<endl;
+
+        if(num%2==0){
+            cout<<"It is a even number"<<endl;
+
+        } else
+            cout<<"It is an odd number"<<endl;
+
+    } else
+        cout<<"It is a negative number";
+
+
+
+
+
+
+
+
+
+
+}
